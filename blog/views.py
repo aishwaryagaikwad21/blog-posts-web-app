3bb4ce3b359e037f66b677cf7c5b4d663290a7e3
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2020-03-04 14:33:38 +0530

    blog2

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
 # Create your views here.
+posts = [
+    {
+        'author':'abc',
+        'title': 'Post 1',
+        'content':'Machine Learing',
+        'data_posted':'March 04,2020'
+    },
+    {
+        'author':'xyz',
+        'title': 'Post 2',
+        'content':'Block Chain',
+        'data_posted':'March 05,2020' 
+    }
+]
 
 def home(request):
-    return render(request, 'home.html') #when user goes to home blog page
+    content = {
+        'posts':posts
+    }
+
+    return render(request, 'home.html',content) #when user goes to home blog page
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html',{'title':'aishwarya'})
 
 def help(request):
     return HttpResponse('<h1>Thanks</h1>')

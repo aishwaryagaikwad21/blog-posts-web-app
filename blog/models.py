7781ee857b42754281c41f31e6b0c79393d7b540
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2020-03-11 19:53:07 +0530

    db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User #one to many relationship.. bcz one user can have multiple posts but single post will have only one author
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)

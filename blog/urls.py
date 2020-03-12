4d90440253a40fd8df586e579f6e3ad0ad981c80
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2020-03-12 22:08:06 +0530

    register3

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-name'), #views.home is functio created in views.py
+    path('home/', views.home, name='blog-home'), #views.home is functio created in views.py
     path('about/', views.about, name='blog-about'),
     path('help/', views.help, name='blog-help'),
     path('base/', views.base,name="blog-base")

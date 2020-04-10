e2aa40963d1c2b04d1e302b2dca706cfd98fac25
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2020-04-10 21:20:02 +0530

    url

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='blog-register'), #views.home is functio created in views.py
+    #path('', views.register, name='blog-register'), #views.home is functio created in views.py
     path('',views.profile, name='user-profile'),
 ]
